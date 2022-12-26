88fbdc74b3c4bc3e7370d897f154c1a5a7fffb44
zainsyed28 <zainsyed664@gmail.com>
2022-12-26 15:52:06 -0800

    added some code to do some mock testing

--- AndroidProjects/app/src/main/java/com/example/myapplication/MainActivity.java
+++ AndroidProjects/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,13 +2,51 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActionBar;
+import android.app.Notification;
+import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.github.sundeepk.compactcalendarview.CompactCalendarView;
+import com.github.sundeepk.compactcalendarview.domain.Event;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+    CompactCalendarView compactCalendar;
+    private SimpleDateFormat dateFormatMonth = new SimpleDateFormat("MMMM - yyyy", Locale.getDefault());
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.secondary);
+
+        compactCalendar = (CompactCalendarView) findViewById(R.id.compactcalendar_view);
+        compactCalendar.setUseThreeLetterAbbreviation(true);
+
+        //Set an event for the dates
+
+        Event ev1 = new Event(Color.BLUE, 1672002000000L, "Christmas");
+        compactCalendar.addEvent(ev1);
+
+        compactCalendar.setListener(new CompactCalendarView.CompactCalendarViewListener() {
+            @Override
+            public void onDayClick(Date dateClicked) {
+                Context context = getApplicationContext();
+                if (dateClicked.toString().compareTo("Fri Dec 25 01:00:00 PST 2022") == 0) {
+                    Toast.makeText(context, "Christmas", Toast.LENGTH_SHORT).show();
+                }else {
+                    Toast.makeText(context, "No Events Planned for that day", Toast.LENGTH_SHORT).show();
+                }
+            }
+            @Override
+            public void onMonthScroll(Date firstDayOfNewMonth) {
+
+            }
+        });
     }
 }
