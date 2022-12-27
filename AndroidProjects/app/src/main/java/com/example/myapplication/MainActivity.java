e336f74804575921b71f1c4e609238ac194aa45a
Keyvan Mahmoodzadeh Kani <arasanjkk@gmail.com>
2022-12-27 14:43:31 -0800

    Added an event box to the main activity

--- AndroidProjects/app/src/main/java/com/example/myapplication/MainActivity.java
+++ AndroidProjects/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -9,6 +9,7 @@ import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.github.sundeepk.compactcalendarview.CompactCalendarView;
@@ -22,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
 
 
     CompactCalendarView compactCalendar;
-    private SimpleDateFormat dateFormatMonth = new SimpleDateFormat("MMMM- yyyy", Locale.getDefault());
+    private SimpleDateFormat dateFormatMonth = new SimpleDateFormat("MMMM - yyyy", Locale.getDefault());
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -31,15 +32,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         final ActionBar actionBar = getSupportActionBar();
-//        actionBar.setDisplayHomeAsUpEnabled(false);
-//        actionBar.setTitle(null);
+        actionBar.setDisplayHomeAsUpEnabled(false);
+        actionBar.setTitle(null);
 
         compactCalendar = (CompactCalendarView) findViewById(R.id.compactcalendar_view);
         compactCalendar.setUseThreeLetterAbbreviation(true);
 
         //Set an event for Teachers' Professional Day 2016 which is 21st of October
 
-        Event ev1 = new Event(Color.WHITE, 1671958800000L, "Christmas");
+        Event ev1 = new Event(Color.WHITE, 1672036846000L, "Christmas");
         compactCalendar.addEvent(ev1);
 
         compactCalendar.setListener(new CompactCalendarView.CompactCalendarViewListener() {
@@ -47,8 +48,10 @@ public class MainActivity extends AppCompatActivity {
             public void onDayClick(Date dateClicked) {
                 Context context = getApplicationContext();
 
-                if (dateClicked.toString().compareTo("Fri Dec 25 00:00:00 PST 2022") == 0) {
+                if (dateClicked.toString().compareTo("Sun Dec 25 10:40:46 PST 2022") == 0) {
                     Toast.makeText(context, "Christmas", Toast.LENGTH_SHORT).show();
+
+                    ((TextView)findViewById(R.id.textbox_forevents)).setText("Merry Christmas!");
                 }else {
                     Toast.makeText(context, "No Events Planned for that day", Toast.LENGTH_SHORT).show();
                 }
