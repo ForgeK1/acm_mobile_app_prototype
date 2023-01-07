27d70d92be5edb7cba77333fe7ab1ae5b5c0531a
ivanh <hoivan08@gmail.com>
2023-01-06 22:38:11 -0800

    Started contact page and homepage.

--- AndroidProjects/app/src/main/java/com/example/myapplication/HomePage.java
+++ AndroidProjects/app/src/main/java/com/example/myapplication/HomePage.java
@@ -1,14 +1,36 @@
 package com.example.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class HomePage extends AppCompatActivity {
 
-    @Override
+    private Button calButton;
+    private Button contButton;
+
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home_page);
+        setContentView(R.layout.home_page);
+        calButton = (Button) findViewById(R.id.calButton);
+        calButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), CalendarUtils.class));
+            }
+        });
+
+        contButton = (Button) findViewById(R.id.contButton);
+        contButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //startActivity(new Intent(getApplicationContext(), CalendarUtils.class));
+            }
+        });
+
     }
 }
