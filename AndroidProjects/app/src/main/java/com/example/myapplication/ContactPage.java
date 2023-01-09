31e3509e343e6605a14200d61b327d6bebaf46d4
Ulises <ulisesgutierrez1217@gmail.com>
2023-01-09 13:28:30 -0800

    Added Logos to the buttons

--- AndroidProjects/app/src/main/java/com/example/myapplication/ContactPage.java
+++ AndroidProjects/app/src/main/java/com/example/myapplication/ContactPage.java
@@ -19,44 +19,54 @@ public class ContactPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.contact_page);
-//        fbButton = (Button) findViewById(R.id.fbButton);
-//        igButton = (Button) findViewById(R.id.igButton);
-//        discordButton = (Button) findViewById(R.id.discordButton);
-//        webButton = (Button) findViewById(R.id.webButton);
+        fbButton = (Button) findViewById(R.id.fbButton);
+        igButton = (Button) findViewById(R.id.igButton);
+        discordButton = (Button) findViewById(R.id.discordButton);
+        webButton = (Button) findViewById(R.id.webButton);
 
-//
-//        fbButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                String url = "https://www.facebook.com/calstatela.acm";
-//
-//                Intent i = new Intent(Intent.ACTION_VIEW);
-//                i.setData(Uri.parse(url));
-//                startActivity(i);
-//            }
-//        });
-//
-//        igButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                String url = "https://www.instagram.com/calstatela_acm/?hl=en";
-//
-//                Intent i = new Intent(Intent.ACTION_VIEW);
-//                i.setData(Uri.parse(url));
-//                startActivity(i);
-//            }
-//        });
-//
-//        discordButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                String url = "https://discordapp.com/invite/wX58JRv";
-//
-//                Intent i = new Intent(Intent.ACTION_VIEW);
-//                i.setData(Uri.parse(url));
-//                startActivity(i);
-//            }
-//        });
+        fbButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "https://www.facebook.com/calstatela.acm";
+
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                startActivity(i);
+            }
+        });
+
+        igButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "https://www.instagram.com/calstatela_acm/?hl=en";
+
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                startActivity(i);
+            }
+        });
+
+        discordButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "https://discordapp.com/invite/wX58JRv";
+
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                startActivity(i);
+            }
+        });
+
+        webButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "https://acm-calstatela.com/";
+
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                startActivity(i);
+            }
+        });
 
 
 
