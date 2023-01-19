12d497074b94ca581712e0bc4f4c496b362a20b3
Ulises <ulisesgutierrez1217@gmail.com>
2023-01-18 17:41:40 -0800

    Created activity for click

---
+++ AndroidProjects/app/src/main/java/com/example/myapplication/Flyer.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Flyer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flyer);
+    }
+}
